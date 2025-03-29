34e58f8378b48940b751c1a5288549b0771a125b
ditahkk <ditah.k@zsoftly.com>
2025-03-29 13:58:53 -0400

    AWS Network Infrastructure Setup

--- aws/terraform/01_networking/cac1/main.tf
+++ aws/terraform/01_networking/cac1/main.tf
@@ -1,3 +1,16 @@
+# versions.tf
+terraform {
+  required_version = ">= 1.7.5"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.5.0, < 7.0.0"
+    }
+  }
+}
+
+# main.tf
 provider "aws" {
   region = "ca-central-1"
 }
